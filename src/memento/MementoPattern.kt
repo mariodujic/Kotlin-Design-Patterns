b6d561cce8251d44c4e344775b398fc9701f35a7
Mario Dujić <liberasoftware@gmail.com>
2020-06-16 06:45:10 +0200

    added memento design pattern

---
+++ src/memento/MementoPattern.kt
@@ -0,0 +1,38 @@
+package memento
+
+data class Memento(val state: String)
+
+class Originator {
+    private lateinit var state: String
+
+    fun setState(state: String) {
+        this.state = state
+    }
+
+    fun create() = Memento(state)
+    fun restore(memento: Memento) {
+        state = memento.state
+        println("New state set in originator $state")
+    }
+}
+
+class Caretaker {
+    private val mementos = mutableListOf<Memento>()
+    fun addMemento(memento: Memento) = mementos.add(memento)
+    fun getMementoByIndex(mementoIndex: Int) = mementos[mementoIndex]
+}
+
+fun main() {
+    val caretaker = Caretaker()
+    val originator = Originator()
+
+    originator.let {
+        it.setState("a1")
+        caretaker.addMemento(it.create())
+        it.setState("a2")
+        caretaker.addMemento(it.create())
+
+        it.restore(caretaker.getMementoByIndex(0))
+        it.restore(caretaker.getMementoByIndex(1))
+    }
+}
