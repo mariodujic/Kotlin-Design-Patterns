333426250477e7ef6d8f9f3b92034446139e0d1b
Mario Dujić <mario.dujic@gmail.com>
2020-07-01 16:13:13 +0200

    added composite pattern

---
+++ src/composite/CompositePattern.kt
@@ -0,0 +1,34 @@
+package composite
+
+interface Shape {
+    fun draw()
+}
+
+class Circle : Shape {
+    override fun draw() {
+        println("Drawing circle")
+    }
+}
+
+class Rectangle : Shape {
+    override fun draw() {
+        println("Drawing rectangle")
+    }
+}
+
+class ShapeComposite {
+    private val shapes = mutableListOf<Shape>()
+
+    fun addShape(vararg shape: Shape) = shapes.forEach { s -> shapes.add(s) }
+    fun useShapes() = shapes.forEach { shape: Shape -> shape.draw() }
+}
+
+fun main() {
+    val circle: Shape = Circle()
+    val rectangle: Shape = Rectangle()
+
+    with(ShapeComposite()) {
+        this.addShape(circle, rectangle)
+        this.useShapes()
+    }
+}
