a8c856b924c347281a6544aa335c5f875663c446
Mario Dujić <mario.dujic@gmail.com>
2020-07-07 17:54:22 +0200

    added flyweight pattern

---
+++ src/flyweight/FlyweightPattern.kt
@@ -0,0 +1,40 @@
+package flyweight
+
+interface Vehicle {
+    val year: Int
+    fun start()
+}
+
+class FordFlyweight(override val year: Int) : Vehicle {
+    override fun start() {
+        println("Starting Ford from year $year")
+    }
+}
+
+class VehicleFactory() {
+    private val vehicles = mutableListOf<Vehicle>()
+
+    fun addVehicle(vararg vehicle: Vehicle) = vehicle.forEach { newVehicle: Vehicle -> vehicles.add(newVehicle) }
+    fun getVehicle(year: Int): Vehicle =
+        vehicles.find { existingVehicle -> existingVehicle.year == year } ?: FordFlyweight(year).also {
+            vehicles.add(it)
+        }
+
+    fun getVehiclesAmount() = apply { println("There are ${vehicles.size} vehicles.") }
+}
+
+fun main() {
+
+    val fordFirst: Vehicle = FordFlyweight(1985)
+    val fordSecond: Vehicle = FordFlyweight(1990)
+    val fordThird: Vehicle = FordFlyweight(1995)
+
+    with(VehicleFactory()) {
+        this.addVehicle(fordFirst, fordSecond, fordThird)
+        this.getVehiclesAmount()
+        this.getVehicle(1985).start()
+        this.getVehiclesAmount()
+        this.getVehicle(2005).start()
+        this.getVehiclesAmount()
+    }
+}
